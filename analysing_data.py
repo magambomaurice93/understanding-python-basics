fd9287f5d2bdac414591c9d653a75f8c25411b34
magambomaurice93 <111293298+magambomaurice93@users.noreply.github.com>
2023-11-15 18:02:39 +0300

    python basics

---
+++ analysing_data.py
@@ -0,0 +1,9 @@
+#saturday, 11th November 2023
+
+#statistics is just an applied mathmatics concept
+#data analysis involves a science of studying raw data to translate data into 
+#meaningful patterns ans conclusions
+
+#statistics involves accurate conclusions about data sets
+
+#
---
+++ statistics session1.py
@@ -0,0 +1,53 @@
+# ANALYZING DATA
+#any row data when collected and organised infrom of numerical format or table is called statistics
+
+#statistics is also the mathmatical study of probability of events occuring based on known 
+# quantitave data or a collection of data 
+# everthing thing in statistics is based on data
+
+# what is involved in numerical statistics
+#1. you need to understand what descriptive statistics
+#2. the types of measures in desriptive statistics
+
+#what is descriptive statistics?
+# its all about describing and summerizing data
+
+#in this process we take two approaches
+    #1. Quantitative way of approach
+    #2. visual way of approach
+    
+# if your suing descriptive statistics, you can apply to 
+
+# while forcuing on statistics you can forcus on
+    #1. univariant -one variable
+    #2. bivariant - two variable
+    #3. multi variant - more variable
+
+# how data is distributed
+#measures taken
+#1. central tendency 
+    #center data, mean median and mode
+    
+#2. correlation or join variability 
+    #Relation btn a pair of variables btn a data set
+    # what we are finding: co- variance and co-efficient correlation
+    
+#3. variability
+    # we shall use variance and S.D
+
+
+#saturday, 11th November 2023
+
+#statistics is just an applied mathmatics concept
+#data analysis involves a science of studying raw data to translate data into 
+#meaningful patterns ans conclusions
+
+#statistics involves accurate conclusions about data sets
+
+#
+
+
+
+
+
+
